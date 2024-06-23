2aadbac9f20211d90b0e1e1b5f95f2f4aca6fb8e
Samuel <samuferfort@gmail.com>
2024-06-23 02:19:58 +0200

    add form and index routers and views

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+router.get("/new", function (req, res) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res) {
+  const { message, user } = req.body;
+  console.log(message, user);
+  const newMessage = {
+    text: message,
+    user: user,
+    added: new Date(),
+  };
+  messages.push(newMessage);
+  res.redirect("/");
+});
+
+module.exports = router;
